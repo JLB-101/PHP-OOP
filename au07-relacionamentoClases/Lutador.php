19d0defe5715db963a1ba0f0c302d6baabbc20e4
Julião LB <juliaolangassitela@gmail.com>
2023-10-22 14:28:22 +0200

    add getts & setts function in Lutador classe

--- au07-relacionamentoClases/Lutador.php
+++ au07-relacionamentoClases/Lutador.php
@@ -29,9 +29,74 @@ class Lutador
     }
 
     # metodos getts and setts
+    public function getNome() {
+        return $this->nome;
+    }
+    public function getNacionalidade() {
+        return $this->nacionalidade;
+    }
+
+    public function getIdade() {
+        return $this->idade;
+    }
+    public function getAltura() {
+        return $this->altura;
+    }
+    public function getPeso() {
+        return $this->peso;
+    }
+
+    public function getCategoria() {
+        return $this->categoria;
+    }
+
+    public function getVitorias() {
+        return $this->vitorias;
+    }
+
+    public function getDerrotas() {
+        return $this->derrotas;
+    }
+    public function getEmpates() {
+        return $this->empates;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function setNacionalidade($nacionalidade) {
+        $this->nacionalidade = $nacionalidade;
+    }
     
+    public function setIdade($idade) {
+        $this->idade = $idade;
+    }
 
+    public function setAltura($altura) {
+        $this->altura = $altura;
+    }
 
+    public function setPeso($peso) {
+        $this->peso = $peso;
+    }
+
+    public function setCategoria($categoria) {
+        $this->categoria = $categoria;
+    }
+
+    public function setVitorias($vitorias) {
+        $this->vitorias = $vitorias;
+    }
+
+    public function setDerrotas($derrotas) {
+        $this->derrotas = $derrotas;
+    }
+
+    public function setEmpate($empates) {
+        $this->empates = $empates;
+    }
+    
     # metodos
     public function apresentar() {}
     public function Status() {}
@@ -41,13 +106,7 @@ class Lutador
     public function perderLuta (){}
 
     public function empatarLuta (){}
-
-
-
-
-
-    
+   
 }
-
 
 ?>
