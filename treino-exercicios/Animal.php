67be2aa443d58b38ade9f4ab69971e28b7bcc90b
Julião LB <juliaolangassitela@gmail.com>
2023-10-23 17:52:08 +0200

    EX3

---
+++ treino-exercicios/Animal.php
@@ -0,0 +1,24 @@
+<?php 
+
+//
+class Animal {
+
+    //Atributos
+    private $especie;
+    private $raca;
+
+    private $cor;
+
+
+    public function andar(){
+        #andar
+    }
+    public function o(){
+
+    //metodo contrutor
+
+    // metodo para ipressao da informacao do funcionario
+
+}
+
+?>
