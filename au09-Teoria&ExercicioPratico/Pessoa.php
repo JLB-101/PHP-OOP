fa56e534a86ece1d2fcb586aefd7d5252baa23b1
Julião LB <juliaolangassitela@gmail.com>
2023-10-27 16:27:57 +0200

    creating People class

---
+++ au09-Teoria&ExercicioPratico/Pessoa.php
@@ -0,0 +1,38 @@
+<?php 
+class Pessoa {
+
+    private $nome;
+    private $idade;
+    private $sexo;
+
+    // getters & setters
+    public function getNome() {
+        return $this->nome;
+    }
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+    public function getIdade() {
+        return $this->idade;
+    }
+    public function setIdade($idade) {
+        $this->idade = $idade;
+    }
+    public function getSexo() {
+        return $this->sexo;
+    }
+    public function setSexo($sexo) {
+        $this->sexo = $sexo;
+    }
+    
+    // metodos especias
+    public function fazerAniversario(){
+        
+    }
+
+
+
+
+
+}
+?>
