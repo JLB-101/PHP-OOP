fe6ce6dfa0c84bf6531510f3d724ce3e8fa1eb36
Julião LB <juliaolangassitela@gmail.com>
2023-10-27 16:39:22 +0200

    add atributs and gets and sets function on Livro

--- au09-Teoria&ExercicioPratico/Livro.php
+++ au09-Teoria&ExercicioPratico/Livro.php
@@ -1,1 +1,59 @@
-php
+<?php 
+class Livro {
+    private $titulo;
+    private $autor;
+    private $toPaginas;
+
+    private $pagAtual;
+    private $aberto;
+
+    private $leitor;
+
+
+    // getters & setters
+
+    public function getTitulo() {
+        return $this->titulo;
+    }
+    public function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+    public function getAutor() {
+        return $this->autor;
+    }
+    public function setAutor($autor) {
+        $this->autor = $autor;
+    }
+    public function getToPaginas() {
+        return $this->toPaginas;
+    }
+    public function setToPaginas($toPaginas) {
+        $this->toPaginas = $toPaginas;
+    }
+    public function getPagAtual() {
+        return $this->pagAtual;
+    }
+    public function setPagAtual($pagAtual) {
+        $this->pagAtual = $pagAtual;
+    }
+    public function getAberto() {
+        return $this->aberto;
+    }
+    public function setAberto($aberto) {
+        $this->aberto = $aberto;
+    }
+    public function getLeitor() {
+        return $this->leitor;
+    }
+    public function setLeitor($leitor) {
+        $this->leitor = $leitor;
+    }
+
+    // functions
+    public function detalhes(){
+        # code
+    }
+
+
+}
+?>
