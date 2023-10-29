54226531926dd427219548d211dce79da5772d2a
Julião LB <juliaolangassitela@gmail.com>
2023-10-29 20:36:43 +0200

    update I

--- au10-Heranca/index.php
+++ au10-Heranca/index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=<, initial-scale=1.0">
     <title>Aula 10</title>
 </head>
+
 <body>
     <h1></h1>
     <?php
@@ -19,20 +21,25 @@
     $p2 = new Aluno();
     $p3 = new Professor();
     $p4 = new Funcionario();
-    
+
     //
     $p1->setNome('John');
     $p2->setNome('Joau');
     $p3->setNome('Chaw');
     $p4->setNome('Matt');
-    
-    
+
+
     //
-    print_r($p1);echo'<br>';
-    print_r($p2);echo'<br>';
-    print_r($p3);echo'<br>';
-    print_r($p4);echo'<br>';
+    print_r($p1);
+    echo '<br>';
+    print_r($p2);
+    echo '<br>';
+    print_r($p3);
+    echo '<br>';
+    print_r($p4);
+    echo '<br>';
 
     ?>
 </body>
+
 </html>
