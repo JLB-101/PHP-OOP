5caf6137da583d74e2a8b1325f8aedbf4472a423
Julião LB <juliaolangassitela@gmail.com>
2023-10-29 15:00:39 +0200

    Pessoa class created

---
+++ au10-Heranca/Pessoa.php
@@ -0,0 +1,37 @@
+<?php
+class Pessoa
+{
+    private $nome;
+    private $idade;
+    private $genero;
+    //
+    public function getNome(){
+        return $this->nome;
+    }
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
+    public function getIdade(){
+        return $this->idade;
+    }
+    public function setIdade($idade){
+        $this->idade = $idade;
+    }
+    public function getGenero(){
+        return $this->genero;
+    }
+    public function setGenero($genero){
+        $this->genero = $genero;
+    }
+
+    // metodo 
+    public function fazerAniv(){
+        $this->getIdade() + 1;
+    }
+
+    
+}
+
+
+
+?>
