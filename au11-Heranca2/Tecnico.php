879603218db5c607a293997f8ade91744dff1ff0
Julião LB <juliaolangassitela@gmail.com>
2023-10-30 00:33:02 +0200

    put info function on Tecnico class

--- au11-Heranca2/Tecnico.php
+++ au11-Heranca2/Tecnico.php
@@ -8,17 +8,35 @@ final class Tecnico extends Aluno
     //Atributos
     private $registroProfissional;
 
+    //construtor
+    public function __construct($nome, $genero, $curso, $registroProfissional)
+    {
+        $this->setNome($nome);
+        $this->setGenero($genero);
+        $this->setCurso($curso);
+        $this->setregistroProfissional($registroProfissional);
+    }
+
     //gets and sets
-    public function getRegistroProfissional(){
+    public function getRegistroProfissional()
+    {
         return $this->registroProfissional;
     }
-    public function setRegistroProfissional($registroProfissional){
+    public function setRegistroProfissional($registroProfissional)
+    {
         $this->registroProfissional = $registroProfissional;
     }
 
     //metodos
-    public function praticar(){
+    public function praticar()
+    {
         echo "{$this->getNome()} esta na Aula Pratica";
+    }
+
+    //
+    public function info()
+    {
+        echo "Nome: {$this->getNome()} estudante do Curso {$this->getCurso()} com numero de Registro {$this->getRegistroProfissional()} ";
     }
 }
 
