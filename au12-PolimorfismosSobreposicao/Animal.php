8528c8539097e39291c4922f617188a6418171fe
Julião LB <juliaolangassitela@gmail.com>
2023-10-31 16:25:49 +0200

    root class: Animal created

---
+++ au12-PolimorfismosSobreposicao/Animal.php
@@ -0,0 +1,25 @@
+<?php
+# imports:
+require_once('');
+
+//class mae:
+abstract class Animal
+{
+    //atributs
+    private $peso;
+    private $idade;
+
+    //metodos
+    public function locomover(){
+        # code
+    }
+
+    public function alimentar(){# code
+    }
+
+    public function emitirSom(){# code
+    }
+
+}
+
+?>
