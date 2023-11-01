0fd20150e289f194beef892d4d279281b09bf65c
Julião LB <juliaolangassitela@gmail.com>
2023-11-01 16:11:42 +0200

    creating index of au14

---
+++ au14-ProjectoFinalPT1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula 14 - Projecto Youtube PT1</title>
+</head>
+<body>
+    <h1></h1>
+    <p></p>
+    <?php 
+    # imports:
+    require_once('');
+
+    //
+
+
+    ?>   
+</body>
+</html>
