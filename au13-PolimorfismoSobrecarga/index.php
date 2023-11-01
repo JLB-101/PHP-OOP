2a5145d530a8d3a3be3941c361b6b787ddd5a35f
Julião LB <juliaolangassitela@gmail.com>
2023-11-01 15:18:06 +0200

    import error fixed

--- au13-PolimorfismoSobrecarga/index.php
+++ au13-PolimorfismoSobrecarga/index.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aula 13 - PolimorfismosSobreCarga</title>
 </head>
+
 <body>
     <h1>Aula 13 - PolimorfismosSobreCarga</h1>
     <hr>
     <br>
 
-    <?php 
-        # imports:
-        require_once('./au12-PolimorfismosSobreposicao/Lobo.php');
-        //
-       require_once('');
+    <?php
+    # imports:
+    require_once('Cachoro.php');
 
-        //
-        $c1 = new Cachoro();
-        
-        $c1->reagir('toma comida');
+    //
+    $c1 = new Cachoro();
+
+    $c1->reagir('toma comida');
     ?>
-    
+
 </body>
+
 </html>
