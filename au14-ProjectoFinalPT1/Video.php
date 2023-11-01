f2a20744624f688160b4acf459acb245d6d07d3d
Julião LB <juliaolangassitela@gmail.com>
2023-11-01 18:23:44 +0200

    adding get & sets and implenting Interface fun()

--- au14-ProjectoFinalPT1/Video.php
+++ au14-ProjectoFinalPT1/Video.php
@@ -13,14 +13,69 @@ class Video implements AcoesVideo {
     private $reproduzindo;
 
     //Contruct
-    
+
 
 
     //gets & sets
+    public function getId(){
+        return $this->id;
+    }
+    public function setId($id) {
+        $this->id = $id;
+    }
+    public function getTitle(){
+        return $this->title;
+    }
+    public function setTitle( $title ) {
+        $this->title = $title;
+    }
+    public function getDescription(){
+        return $this->description;
+    }
+    public function setDescription( $description ) {
+        $this->description = $description;
+    }
+    public function getAvaliacao(){
+        return $this->avaliacao;
+    }
+    public function setAvaliacao( $avaliacao ) {
+        $this->avaliacao = $avaliacao;
+    }
+    public function getView(){
+        return $this->view;
+    }
+    public function setView( $view ) {
+        $this->view = $view;
+    }
+    public function getCurtida(){
+        return $this->curtida;
+    }
+    public function setCurtida( $curtida ) {
+        $this->curtida = $curtida;
+    }
+    public function getReproduzindo(){
+        return $this->reproduzindo;
+    }
+    public function setReproduzindo( $reproduzindo ){
+        $this->reproduzindo =$reproduzindo;
+    }
+    
 
 
     //metodos (function)
+    public function play(){
+
+    }
+    public function pause(){
+
+    }
+    public function like(){
+
+    }
+    public function dislike(){
+
+    }
 
 
-
+}
 ?>
