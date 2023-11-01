de926ffca6f57a07bf4f555de47a01e70ce2ef7b
Julião LB <juliaolangassitela@gmail.com>
2023-11-01 18:11:24 +0200

    creating Video class

---
+++ au14-ProjectoFinalPT1/Video.php
@@ -0,0 +1,25 @@
+<?php
+
+class Video implements AcoesVideo {
+
+    //Atributos
+    private $i = 1;
+    private $id;
+    private $title;
+    private $description;
+    private $avaliacao;
+    private $view;
+    private $curtida;
+    private $reproduzindo;
+
+    //Contruct
+
+
+    //gets & sets
+
+
+    //metodos (function)
+
+
+
+?>
