73e1e2e40ac16a0fe10ab5a270347f15eb4384a0
Julião LB <juliaolangassitela@gmail.com>
2023-11-01 19:17:44 +0200

    update class Gafanhoto

--- au14-ProjectoFinalPT1/Gafanhoto.php
+++ au14-ProjectoFinalPT1/Gafanhoto.php
@@ -1,27 +1,44 @@
-<?php 
+<?php
+# imports:
+require_once('Pessoa.php');
 
-class Gafanhoto extends Pessoa {
+class Gafanhoto extends Pessoa
+{
     //Atributs
     private $login;
     private $totAsistido;
-   
+
+    //Contructor -- getting from Pessoa || herdando da Pessoa
+    public function __construct($nome, $idade, $sexo, $login, )
+    {
+        //super($nome, $idade, $sexo);
+        parent::__construct($nome, $idade, $sexo);
+        $this->login = $login;
+        $this->totAsistido = 0;
+    }
+
     //gets & sets
-    public function getLogin(){
+    public function getLogin()
+    {
         return $this->login;
     }
-    public function setLogin($login){
+    public function setLogin($login)
+    {
         $this->login = $login;
     }
-    public function getTotAsistido(){
+    public function getTotAsistido()
+    {
         return $this->totAsistido;
     }
-    public function setTotAsistido($totAsistido){
-        $this->totAsistido = (int)$totAsistido;
+    public function setTotAsistido($totAsistido)
+    {
+        $this->totAsistido = (int) $totAsistido;
     }
 
     //
-    public function viuMaisUm(){
-
+    public function assistirMaisUm()
+    {
+        $this->totAsistido++;
     }
 
 
